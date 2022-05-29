ac0b19b794b82d58d2d57103c84d3f7c9b4c7578
Hoshuatt <joshualimbautista@gmail.com>
2022-05-28 21:43:36 -0400

    Initial Release
    First release

---
+++ VolleyballTeamsGenerator.py
@@ -0,0 +1,68 @@
+import random
+
+teams = int(input("How many teams are needed?\n"))
+
+players = []
+
+def TeamRandomizer():
+    with open('Volleyball.txt', 'r') as f:
+        
+        for line in f:
+            players.append(line.replace("\n",""))
+        random.shuffle(players)
+
+def TeamGenerator():
+
+    if (teams < 2):
+            exit("Invalid Input: Amount of Teams must be greater than or equal to 2")
+
+    elif (teams == 2):
+        with open('Volleyball.txt', 'r') as f:
+        
+            for line in f:
+                players.append(line.replace("\n",""))
+            random.shuffle(players)
+            print(players[0:6],"\n")
+            print(players[7:12],"\n")
+
+    elif (teams == 3):
+        with open('Volleyball.txt', 'r') as f:
+        
+            for line in f:
+                players.append(line.replace("\n",""))
+            random.shuffle(players)
+            print(players[0:6],"\n")
+            print(players[7:12],"\n")
+            print(players[13:18],"\n")
+
+    elif (teams > 3):
+        exit("Invalid Input: Amount of Teams must be less than or equal to 3")
+
+        
+TeamGenerator()
+print("\nHere is a list of teams:\n")
+
+redo = input("Are you happy with these teams? Enter y or n\n")
+if (redo == 'y'):
+    print("Have fun!")
+else:
+    while(redo == 'n'):
+        players = []
+        TeamGenerator()
+        rerun = input("Rerun again? Enter y or n\n")
+        if (rerun == 'y'):
+            continue
+        else:
+            print("Have fun!")
+            break
+
+
+
+
+
+
+
+
+
+
+
